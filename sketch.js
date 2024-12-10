160c7f2c4dda9c439215e98f6d1b700b5272bae1
Ivy-langley <ivy.mary.langley@gmail.com>
2024-12-10 16:28:35 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,19 @@
 function setup() {
   createCanvas(400, 400);
   noStroke();
-  fill("yellow");
+  
 }
 
 function draw() {
   background(220);
   //iz, draw a little smiley face or a shape or something;
+  fill("yellow");
   circle((width/2), (height/2), 100);
+  fill("yellow");
   //ivy, add a face on top of my circle!
+  fill('black');
+  circle(180, 190, 10);
+  circle(220, 190, 10);
+  //paris draw the expression !
+  }
 
-}
