75778f73efa6636f215287a779d98de27cad72c0
izandrews <isabelandrews101@gmail.com>
2024-12-10 16:20:39 +0000

    Ivy you're up!

--- sketch.js
+++ sketch.js
@@ -1,8 +1,13 @@
 function setup() {
   createCanvas(400, 400);
+  noStroke();
+  fill("yellow");
 }
 
 function draw() {
   background(220);
   //iz, draw a little smiley face or a shape or something;
+  circle((width/2), (height/2), 100);
+  //ivy, add a face on top of my circle!
+
 }
